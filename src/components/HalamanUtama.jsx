69b7f85c83c0c7b5d4e5a6437f225753d13cc0c2
lirarahmawati20 <lirarahmawati94@gmail.com>
2024-09-13 09:39:45 +0700

    update3

--- src/components/HalamanUtama.jsx
+++ src/components/HalamanUtama.jsx
@@ -5,7 +5,7 @@ export default function HalamanUtama() {
     <>
       <header className="bg-gray-800 text-white p-5 font-serif">
         <div className="max-w-4xl mx-auto flex flex-wrap justify-between items-center">
-          <h1 className="text-4xl font-bold">Lira.com</h1>
+          {/* <h1 className="text-4xl font-bold">Lira.com</h1> */}
 
           {/* Navigation */}
           <nav className="w-full md:w-auto">
